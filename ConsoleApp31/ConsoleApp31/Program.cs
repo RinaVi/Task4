4b349f6c84b074259e2c90b878fa28541ce0b91e
Katherine <katevi33@gmail.com>
2018-05-16 22:17:51 +0300

    16.05.2018 first commit

---
+++ ConsoleApp31/ConsoleApp31/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var dirPath = @"k:";
+            var directories = Directory.EnumerateDirectories(dirPath, "*", SearchOption.TopDirectoryOnly);
+            try
+            {
+                foreach (var item in directories)
+                {
+                    Console.WriteLine(item);
+                    foreach (var folder in Directory.GetDirectories(item))
+                        Console.WriteLine("1" + folder);
+                }
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+
+            };
+            
+            Console.ReadLine();
+        }
+    }
+    
+}
